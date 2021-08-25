6c212337684332955070426f1b86287d69168507
Bruno <mguimaraesbruno@gmail.com>
2021-08-25 08:37:36 -0300

    Code Review: Brunão

--- src/postStructure/SendingTime.js
+++
@@ -1,7 +0,0 @@
-export default function SendingTime(props) {
-    return (
-        <div class="time-since-sent small-text light-gray">
-            Há {props.timeSinceSent}
-        </div>
-    );
-}
